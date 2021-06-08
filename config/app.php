67de7fec8fcc37297a53d0021792888954022b64
Gerza <gerza@paperstreetmedia.com>
2021-06-07 23:50:30 -0400

    ok

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         //AvoRed\Graphql\GraphqlProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
